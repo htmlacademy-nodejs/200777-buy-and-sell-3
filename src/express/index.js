ee3e1a1fba97d5d7ee7e1a0d8894cd5101c73645
Leks <leks511@bk.ru>
2020-11-27 21:02:47 +0300

    Внёс правки после общения с наставником

--- src/express/index.js
+++ src/express/index.js
@@ -13,16 +13,19 @@ const PUBLIC_DIR = `public`;
 const app = express();
 
 
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
 
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use((req, res) => res.status(404).render(`errors/404`));
-app.use((req, res) => res.status(500).render(`errors/500`));
+app.use((err, req, res, _next) => res.status(500).render(`errors/500`));
 
-app.set(`views`, path.resolve(__dirname, `templates`));
-app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер работает на ${DEFAULT_PORT}`));
